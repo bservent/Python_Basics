8621281a32bd6ab3e1a8749b812e1cf30afda416
Brittany Servent <bservent@gmail.com>
2021-03-31 15:58:21 -0700

    type() function python

---
+++ syntax.py
@@ -0,0 +1,21 @@
+#variable is just a storage unit(think shopping cart where you put in apple and then twitch it to a banana)
+
+#########################################################
+
+#DATA TYPES IN PYTHON
+
+#Immutable = strings, numbers, tuples
+#mutable = dictionaries, lists, sets
+
+#Numbers = integers, floating point numbers, or complex numbers
+
+num1=100
+print(type(num1))
+
+#example above is an integer
+
+###type() is a built in function in Python that takes in either one or three parameters. In the example above it is taking in the variable and checking what its associate data structure is (<class 'int'>). It is usually not used for testing. isinstance() is used in testing because it takes into account whether the given object is an instance of a subclass as well.
+
+
+
+
